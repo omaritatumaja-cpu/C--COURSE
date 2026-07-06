7b94745f1c69f52488e3605c05b2ada9c0714386
ABDOUL KARENZO <mafunzoali@gmail.com>
2026-07-06 16:43:48 +0300

    added

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyFirstProgram;
+
+// Using a Primary Constructor to define the 'speed' property directly
+class Car(int speed)
+{
+    private int _speed = speed;
+
+    public int Speed
+    {
+        get => _speed; // Expression-bodied getter
+        set => _speed = value > 500 ? 500 : value; // Expression-bodied setter with ternary logic
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var car = new Car(400);
+        car.Speed = 1000000000;
+        Console.WriteLine(car.Speed); // Output: 500
+    }
+}
